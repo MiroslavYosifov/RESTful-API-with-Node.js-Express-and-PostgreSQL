161982939da052f980ad3f1f4368e15e33b3a1c7
unknown <m.yosifov193@gmail.com>
2021-02-20 02:52:25 +0200

    updated server replace PostreSQL with MongoDB

--- Server-Side/routes/food.js
+++ Server-Side/routes/food.js
@@ -4,7 +4,7 @@ import { foodControllers } from '../controllers/index.js';
 const router = Router();
 
 router.get('/', foodControllers.get.list);
-router.post('/', foodControllers.get.pagination); //should be get method
+router.post('/page', foodControllers.get.pagination); //should be get method
 router.post('/', foodControllers.post.add);
 router.put('/', foodControllers.put.updateOne);
 router.delete('/', foodControllers.delete.deleteOne);
